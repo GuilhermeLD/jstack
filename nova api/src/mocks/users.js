98de2e43102fdc4d510c44fe88a6fa44a1532ff6
unknown <guilherme_limdavid@yahoo.com.br>
2023-08-18 00:55:32 -0300

    fazendo tudo de novo

---
+++ nova api/src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Guilherme'
+  },
+  {
+    id: 2,
+    name: 'Lima'
+  },
+  {
+    id: 3,
+    name: 'David'
+  },
+  {
+    id: 4,
+    name: 'Guizudo'
+  }
+];
---
+++ nova api/src/routes.js
@@ -0,0 +1,13 @@
+const userController = require('./controllers/userController');
+
+module.exports = [
+  {
+    endpoint: '/users',
+    method: 'GET',
+    handler: userController.listUsers,
+  },{
+    endpoint: '/users/:id',
+    method: 'GET',
+    handler: userController.getUserById,
+  },
+];
